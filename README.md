ba68dc8362420b8e5c9239373e83b1ca91027cc3
Sandy McCabe <sandytech13@gmail.com>
2021-10-10 16:39:39 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler 
+
+## User Stories
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Website
+
+The website address is https://sandymccabe.github.io/dailySchedule/
+
+![alt text](./assets/images/Scheduler.JPG "Scheduler")
+
+## Credits
+Starter code provided by Trilogy Education Services
+
+## Technologies 
+Technologies used in the making of this site include HTML, CSS, DOM Manipulation, JavaScript and JQuery.
+
+## License
+There is currently no license applied to this website.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,79 +1,79 @@
 // Add date and time to Header
 $(document).ready(function () {
 
-// variables to store the time, and also the "hour" in military format
-var currentTime = moment().format('LTS');
-var currentHour = moment().format('kk');
+  // variables to store the time, and also the "hour" in military format
+  var currentTime = moment().format('LTS');
+  var currentHour = moment().format('kk');
 
-// variable to hold our daily planner
-var scheduleArr = [];
+  // variable to hold our daily planner
+  var scheduleArr = [];
 
-//display current date and time
-$("#currentDay").text(moment().format('dddd, MMMM Do YYYY'));
-$("#currentTime").text(moment().format('LTS'));
+  //display current date and time
+  $("#currentDay").text(moment().format('dddd, MMMM Do YYYY'));
+  $("#currentTime").text(moment().format('LTS'));
 
-//call function to color calendar based on time
-addColor();
+  //call function to color calendar based on time
+  addColor();
 
-//call function to render the tasks on page load
-renderPage();
+  //call function to render the tasks on page load
+  renderPage();
 
 
-function renderPage() {
-  
-  scheduleArr = JSON.parse(localStorage.getItem("Arr") );
-  console.log(scheduleArr);
+  function renderPage() {
 
-  // if it's emptly, initiaze it
-  if (!scheduleArr) {
-    scheduleArr = [];
-  };
-  
-  //if it's not empty, load up the page
-  if (scheduleArr) {
-    for(i = 0; i < scheduleArr.length; i++) {
-      var dailyTasks = (scheduleArr[i].time);
-      
-      var tgtBlock = document.getElementById(scheduleArr[i].time).querySelector(".task");      
-      tgtBlock.innerHTML =  scheduleArr[i].task;
-      
-    }
-  }
-       
-    }
-    
-        
-    // add color to calendar
-    function addColor() {
-      var taskBlock = document.getElementsByClassName("task");
-      for (i = 0; i < taskBlock.length; i++) {
-        if (i + 8 == currentHour) {
-          //if the time block is the current hour, then background is red
-          taskBlock[i].style.backgroundColor = '#F5B7B1';
-        } else if (i + 8 > currentHour) {
-          //if the time block is greater than the current hour, future, then green
-          taskBlock[i].style.backgroundColor = '#ABEBC6';
-        } else {
-          //all other cases means that the time has passed, so grey
-          taskBlock[i].style.backgroundColor = '#D5D8DC';
-        }
-      }
+    scheduleArr = JSON.parse(localStorage.getItem("Arr"));
+    console.log(scheduleArr);
+
+    // if it's emptly, initiaze it
+    if (!scheduleArr) {
+      scheduleArr = [];
     };
-    
-  
-  
+
+    //if it's not empty, load up the page
+    if (scheduleArr) {
+      for (i = 0; i < scheduleArr.length; i++) {
+        var dailyTasks = (scheduleArr[i].time);
+
+        var tgtBlock = document.getElementById(scheduleArr[i].time).querySelector(".task");
+        tgtBlock.innerHTML = scheduleArr[i].task;
+
+      }
+    }
+
+  }
+
+
+  // add color to calendar
+  function addColor() {
+    var taskBlock = document.getElementsByClassName("task");
+    for (i = 0; i < taskBlock.length; i++) {
+      if (i + 8 == currentHour) {
+        //if the time block is the current hour, then background is red
+        taskBlock[i].style.backgroundColor = '#F5B7B1';
+      } else if (i + 8 > currentHour) {
+        //if the time block is greater than the current hour, future, then green
+        taskBlock[i].style.backgroundColor = '#ABEBC6';
+      } else {
+        //all other cases means that the time has passed, so grey
+        taskBlock[i].style.backgroundColor = '#D5D8DC';
+      }
+    }
+  };
+
+
+
   $('.btn').click(function () {
-  
+
     var newTaskBlock = $(this).siblings('.task').val();
     var newTime = $(this).parent().attr('id');
-  
+
     var obj = {
       "time": newTime,
       "task": newTaskBlock
     };
-        
+
     scheduleArr.push(obj);
-    
+
     localStorage.setItem("Arr", JSON.stringify(scheduleArr));
   });
 
