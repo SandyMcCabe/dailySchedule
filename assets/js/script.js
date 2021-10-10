224373918ddd452b28dd81dcc6a08f91318b283f
Sandy McCabe <sandytech13@gmail.com>
2021-10-09 21:01:08 -0500

    cleaned up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ $(document).ready(function () {
 var currentTime = moment().format('LTS');
 var currentHour = moment().format('kk');
 
+// variable to hold our daily planner
 var scheduleArr = [];
 
 //display current date and time
@@ -13,75 +14,35 @@ $("#currentTime").text(moment().format('LTS'));
 
 //call function to color calendar based on time
 addColor();
+
+//call function to render the tasks on page load
 renderPage();
-// allStorage();
-
-// scheduleArr = JSON.parse(localStorage.getItem(Key)), JSON.parse(localStorage.getItem(Value)) || [] ;
-// console.log(scheduleArr);
-// loadMeetings();
-
-
-  // load tasks from local storage
-// function loadMeetings() {
-//     var meetingsArr = JSON.parse(localStorage.getItem("tasks"));
-    // for(let i = 0; i < localStorage.length; i++) {
-    //   const time = localStorage.key(i);
-    //   const task = localStorage.value(key);
-
-    //   scheduleArr.push(time, task);
-    //   console.log(scheduleArr);
-
-    // }
-//     console.log(meetingsArr)
-//     // if nothing in localStorage, create a new object to track all task status arrays
-//     if (!meetingsArr) {
-//       meetingsArr = {
-//         Time: [],
-//         Task: []
-//       };
-//     }
-//     console.log(meetingsArr);
-//   };
-  
-// save new tasks into Local Storage
 
 
 function renderPage() {
-  console.log("1");
-  console.log(localStorage);
   
   scheduleArr = JSON.parse(localStorage.getItem("Arr") );
-  console.log("2");
-  
   console.log(scheduleArr);
-  
-  console.log("3");
 
+  // if it's emptly, initiaze it
   if (!scheduleArr) {
     scheduleArr = [];
   };
   
+  //if it's not empty, load up the page
   if (scheduleArr) {
     for(i = 0; i < scheduleArr.length; i++) {
       var dailyTasks = (scheduleArr[i].time);
       
       var tgtBlock = document.getElementById(scheduleArr[i].time).querySelector(".task");      
-      // var tgtBlock = tgtParent.querySelector('task');
       tgtBlock.innerHTML =  scheduleArr[i].task;
-      
-      console.log("inner");
-      // var ArrBlock = $(this).siblings('.task').val();
-      // var newTime = $(this).parent().attr('id');
       
     }
   }
-     
-  
+       
     }
     
-    
-    
-    
+        
     // add color to calendar
     function addColor() {
       var taskBlock = document.getElementsByClassName("task");
@@ -106,21 +67,14 @@ function renderPage() {
     var newTaskBlock = $(this).siblings('.task').val();
     var newTime = $(this).parent().attr('id');
   
-  console.log(newTime);
-  console.log(newTaskBlock)
-  
     var obj = {
       "time": newTime,
       "task": newTaskBlock
     };
-  
-    console.log(obj);
-  
+        
     scheduleArr.push(obj);
-    console.log(scheduleArr);
-  
+    
     localStorage.setItem("Arr", JSON.stringify(scheduleArr));
   });
-
 
 });
